860fe5de10e407364765b48f7db4adb84c10a925
xDavidLeon <david@linceworks.com>
2019-01-26 18:00:40 +0100

    WIP Player Systems

--- GGJ19_Unity/GGJ19/Assets/_Project/Scripts/PlayerController.cs
+++ GGJ19_Unity/GGJ19/Assets/_Project/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [System.Serializable]
+    public class PlayerInput
+    {
+        public float posX = 0;
+        public float posZ = 0;
+        public float placementX = 0;
+        public float placementZ = 0;
+        public Transform pointer;
+    };
+
     [Header("Block Selection")]
     public Block selectedBlock;
     public PlayBlock playBlock;
@@ -12,9 +22,7 @@ public class PlayerController : MonoBehaviour
     public float joySpeed = 15.0f;
     public float offsetX = -2.5f;
     public float offsetY = -2.5f;
-
-    private float posX = 0;
-    private float posZ = 0;
+    public List<PlayerInput> playerInputs;
 
     private Plane plane;
 
@@ -40,67 +48,75 @@ public class PlayerController : MonoBehaviour
         if (!cam || !playBlock)
             return;
 
-        float placementX = posX;
-        float placementZ = posZ;
+        for(int i = 0; i < GameManager.Instance.numPlayers; i++)
+            UpdatePlayerInput(playerInputs[i]);
+    }
 
-        float mouseMag = Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y"));
-        float joyH = Input.GetAxis("Horizontal");
-        float joyV = Input.GetAxis("Vertical");
+    public void UpdatePlayerInput(PlayerInput pInput)
+    {
+        pInput.placementX = pInput.posX;
+        pInput.placementZ = pInput.posZ;
 
-        if (mouseMag > Mathf.Epsilon) usingMouse = true;
-        else if (Mathf.Abs(joyH) + Mathf.Abs(joyV) > Mathf.Epsilon) usingMouse = false;
-
-        if (usingMouse) {
+        int pIndex = playerInputs.IndexOf(pInput);
+        bool myTurn = pIndex == GameManager.Instance.currentPlayer; 
+        if (pIndex == 0)
+        {
+            float mouseMag = Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y"));
             Ray ray = cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
             if(Physics.Raycast(ray, out hit, 100))
             {
                 Vector3 hitPoint = hit.point;
                 Debug.DrawRay(ray.origin, hitPoint - ray.origin, Color.green);
 
-                posX = Mathf.Clamp(Mathf.Round(hitPoint.x - 0.5f), 0.0f, GameManager.Instance.boardWidth - 1) + 0.5f + offsetX;
-                posZ = Mathf.Clamp(Mathf.Round(hitPoint.z - 0.5f), 0.0f, GameManager.Instance.boardHeight - 1) + 0.5f + offsetY;
+                pInput.posX = Mathf.Clamp(Mathf.Round(hitPoint.x - 0.5f), 0.0f, GameManager.Instance.boardWidth - 1) + 0.5f + offsetX;
+                pInput.posZ = Mathf.Clamp(Mathf.Round(hitPoint.z - 0.5f), 0.0f, GameManager.Instance.boardHeight - 1) + 0.5f + offsetY;
 
-                placementX = posX;
-                placementZ = posZ;
+                pInput.placementX = pInput.posX;
+                pInput.placementZ = pInput.posZ;
             }
             else
                 Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
         }
         else
         {
-            //Vector3 moveDirection = cam.transform.forward * joyV + cam.transform.right * joyH;
+            float joyH = Input.GetAxis("Horizontal");
+            float joyV = Input.GetAxis("Vertical");
+            pInput.posX += joyH * Time.deltaTime * joySpeed;
+            pInput.posZ += joyV * Time.deltaTime * joySpeed;
 
-            posX += joyH * Time.deltaTime * joySpeed;
-            posZ += joyV * Time.deltaTime * joySpeed;
-
-            placementX = Mathf.Clamp(Mathf.Round(posX - 0.5f), 0.0f, GameManager.Instance.boardWidth - 1) + 0.5f + offsetX;
-            placementZ = Mathf.Clamp(Mathf.Round(posZ - 0.5f), 0.0f, GameManager.Instance.boardHeight - 1) + 0.5f + offsetY;
+            pInput.placementX = Mathf.Clamp(Mathf.Round(pInput.posX - 0.5f), 0.0f, GameManager.Instance.boardWidth - 1) + 0.5f + offsetX;
+            pInput.placementZ = Mathf.Clamp(Mathf.Round(pInput.posZ - 0.5f), 0.0f, GameManager.Instance.boardHeight - 1) + 0.5f + offsetY;
         }
+
+        pInput.pointer.position = Vector3.Lerp(pInput.pointer.position, new Vector3(pInput.placementX + 2, Constants.boardPlayblockHeight + 0.5f, pInput.placementZ + 2), Time.deltaTime * 4);
 
         // Move the playBlock to the target position
-        playBlock.transform.position = new Vector3(placementX, Constants.boardPlayblockHeight, placementZ);
-
-        if (Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump"))
+        if(myTurn)
         {
-            Vector3 placementPosition = new Vector3(placementX, Constants.boardPlayblockHeight, placementZ);
-            playBlock.transform.position = placementPosition;
+            playBlock.transform.position = new Vector3(pInput.placementX, Constants.boardPlayblockHeight, pInput.placementZ);
 
-            Debug.DrawLine(placementPosition, placementPosition + Vector3.up, Color.blue);
-
-            if(GameManager.Instance.PlacePlayBlock(playBlock))
+            if(Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump"))
             {
-                // Get new block
-                playBlock.SetData(GameManager.Instance.blockDatabase.GetRandomBlock(), Board.GetRandomRoomType(), GameManager.Instance.currentPlayer);
-                playBlock.Populate();
+                Vector3 placementPosition = new Vector3(pInput.placementX, Constants.boardPlayblockHeight, pInput.placementZ);
+                playBlock.transform.position = placementPosition;
 
-                // TODO if it was a valid placement, check if game is over. If not, give control to next player
-                GameManager.Instance.NextPlayer();
+                Debug.DrawLine(placementPosition, placementPosition + Vector3.up, Color.blue);
+
+                if(GameManager.Instance.PlacePlayBlock(playBlock))
+                {
+                    // Get new block
+                    playBlock.SetData(GameManager.Instance.blockDatabase.GetRandomBlock(), Board.GetRandomRoomType(), GameManager.Instance.currentPlayer);
+                    playBlock.Populate();
+
+                    // TODO if it was a valid placement, check if game is over. If not, give control to next player
+                    GameManager.Instance.NextPlayer();
+                }
+            }
+            else if(Input.GetMouseButtonDown(1))
+            {
+                playBlock.Rotate(1);
             }
         }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            playBlock.Rotate(1);
-        }
-
+        
     }
 }
